ac7539910c2002c97ee567a21248e5ad945e87ee
Denzel <dbartolaba123@gmail.com>
2022-01-27 17:37:50 -0800

    Got my fight portion finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [moonSize, setMoonSize] = useState(1);
+  const [sunSize, setSunSize] = useState(1);
+
+  const moonStyle = {
+    fontSize: `${moonSize}rem`
+  };
+  const sunStyle = {
+    fontSize: `${sunSize}rem`
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='fight'>
+        <div className='moon'>
+          <p style={moonStyle}>🌚</p>
+          <button onClick={() => setMoonSize(moonSize + 1)}>Moon powers ACTIVATE!!!</button>
+          <button onClick={() => setSunSize(sunSize - 1)}>Moon blocks the sun!</button>
+
+     
+        </div>
+        <div className='sun'>
+          <p style={sunStyle}>🌞</p>
+          <button onClick={() => setSunSize(sunSize + 1)}>Sun powers ACTIVATE!!!</button>
+          <button onClick={() => setMoonSize(moonSize - 1)}>Sun shines on the moon!</button>
+     
+        </div>
+      </section>
+      <div className='open-close'>
+
+     
+      </div>
+      <div className='zoo'>
+
+     
+      </div>
+
     </div>
   );
 }
